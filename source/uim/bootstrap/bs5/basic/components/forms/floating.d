c4bc26f066618086c88bd2b9fcef22cd4f484e5b
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-07-10 09:11:13 +0200

    Update

--- source/uim/bootstrap/bs5/basic/components/forms/floating.d
+++ source/uim/bootstrap/bs5/basic/components/forms/floating.d
@@ -3,12 +3,16 @@ module uim.bootstrap.bs5.basic.components.forms.floating;
 @safe: 
 import uim.bootstrap;
 
-class DBS5FormFloatting : DBS5Obj {
+class DBS5FormFloating : DBS5Obj {
 	mixin(H5This!("Div", ["form-floating"]));
+
+  O label(this O)(string forElement, string text) {
+    BS5FormFloating(H5Label(["for":forElement], text));
+    return cast(O)this;
+  }
 }
-mixin(H5Calls!"BS5FormFloatting");
+mixin(H5Calls!"BS5FormFloating");
 
 unittest {
-	assert(Assert(BS5FormFloatting, `<form></form>`));
-	assert(Assert(BS5FormFloatting.inline, `<form class="form-inline"></form>`));
-}<div class="
+	assert(Assert(BS5FormFloating, `<div class="form-floating"></div>`));
+}
