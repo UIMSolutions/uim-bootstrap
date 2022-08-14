de17c1a91eab0698f96ace3c681dc67802bbd258
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-14 09:40:54 +0200

    Update button.d

--- source/uim/bootstrap/bs5/basic/components/forms/controls/button.d
+++ source/uim/bootstrap/bs5/basic/components/forms/controls/button.d
@@ -4,10 +4,14 @@
 import uim.bootstrap; 
 
 class DBS5InputButton : DBS5Input {
-	mixin(H5This!(null, null, `["type":"button"]`));
+  mixin(H5This!(null, null, `["type":"button"]`));
+
+  override void initialize () {
+    super.initialize();
+  }
 }
 mixin(H5Calls!("BS5InputButton"));
 unittest {
-	assert(BS5InputButton);
-	assert(BS5InputButton == `<input class="form-control" type="button">`);
+  assert(BS5InputButton);
+  assert(BS5InputButton == `<input class="form-control" type="button">`);
 }
