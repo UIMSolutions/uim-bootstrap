e9a71c53c6fd9bee84ccba5c336ee01b9c1db707
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-14 09:07:30 +0200

    Update check.d

--- source/uim/bootstrap/bs5/basic/components/forms/check.d
+++ source/uim/bootstrap/bs5/basic/components/forms/check.d
@@ -8,14 +8,13 @@ class DBS5FormCheck : DBS5Obj {
 	mixin(TProperty!("string", "forId"));
 
 	O inline(this O)(bool mode = true) { if (mode) this.classes("form-check-inline"); return cast(O)this; }
-	unittest {
-		assert(BS5FormCheck.inline == `<div class="form-check form-check-inline"></div>`);
-	}
-
+	
 	mixin(MyContent!("label", "BS5FormCheckLabel"));
 }
 mixin(H5Calls!"BS5FormCheck");
 
 unittest {
 	assert(BS5FormCheck == `<div class="form-check"></div>`);
+  assert(BS5FormCheck.inline == `<div class="form-check form-check-inline"></div>`);
+	
 }
