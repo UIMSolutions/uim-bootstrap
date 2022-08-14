fe1b3af26d7d568795f8a1608abafb6a043110d4
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-14 10:02:45 +0200

    Update plaintext.d

--- source/uim/bootstrap/bs5/basic/components/forms/controls/plaintext.d
+++ source/uim/bootstrap/bs5/basic/components/forms/controls/plaintext.d
@@ -4,10 +4,14 @@
 import uim.bootstrap; 
 
 class DBS5InputPlaintext : DBS5Input {
-	mixin(H5This!("", ["form-control-plaintext"], `["type":"text"]`));
+  mixin(H5This!("", ["form-control-plaintext"], `["type":"text"]`));
+
+  override void initialize () {
+    super.initialize();
+  }
 }
 mixin(H5Calls!("BS5InputPlaintext"));
-///
+
 unittest {
-//	assert(BS5InputPlaintext, `<input class="form-control" type="text">`);
+	assert(BS5InputPlaintext == `<input class="form-control" type="text">`);
 }
