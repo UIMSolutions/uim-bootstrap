304a49e44fc90b1948fa54cdc08671826e07a30f
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-14 09:44:48 +0200

    Update datetime.d

--- source/uim/bootstrap/bs5/basic/components/forms/controls/datetime.d
+++ source/uim/bootstrap/bs5/basic/components/forms/controls/datetime.d
@@ -6,30 +6,28 @@ import uim.bootstrap;
 class DBS5InputDateTime : DBS5Input {
 	mixin(H5This!("", null, `["type":"datetime-local"]`));
 
+  override void initialize () {
+    super.initialize();
+  }
+
 	O value(this O)(long timestamp) {
 		_attributes["value"] = (cast(DateTime)fromTimestamp(timestamp)).toISOExtString;
 		return cast(O)this;
 	}
-	unittest {	
-
-	}
-
+	
 	O value(this O)(SysTime systime) {
 		_attributes["value"] = (cast(DateTime)systime).toISOExtString;
 		return cast(O)this;
-	}
-	unittest {		
 	}
 
 	O value(this O)(DateTime datetime) {
 		_attributes["value"] = datetime.toISOExtString;
 		return cast(O)this;
 	}
-	unittest {		
-	}
+	
 }
 mixin(H5Calls!("BS5InputDateTime"));
-///
+
 unittest {
-	assert(BS5InputDateTime, `<input class="form-control" type="datetime-local">`);
+	assert(BS5InputDateTime == `<input class="form-control" type="datetime-local">`);
 }
