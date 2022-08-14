95ac97517f366ecbe7dfc3abeb6e9c59a227240b
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-14 10:11:20 +0200

    Update submit.d

--- source/uim/bootstrap/bs5/basic/components/forms/controls/submit.d
+++ source/uim/bootstrap/bs5/basic/components/forms/controls/submit.d
@@ -5,13 +5,13 @@ import uim.bootstrap;
 
 class DBS5InputSubmit : DBS5Input {
 	mixin(H5This!("Input", ["form-control"], `["type":"submit"]`));
-	override public void initialize() {
+	override void initialize() {
 		super.initialize;
 		_single = true;
 	}
 }
 mixin(H5Calls!("BS5InputSubmit"));
-///
+
 unittest {
-	assert(BS5InputSubmit, `<input class="form-control" type="submit">`);
+  assert(BS5InputSubmit == `<input class="form-control" type="submit">`);
 }
