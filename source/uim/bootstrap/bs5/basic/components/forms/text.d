021aba7cfa748cf65ed3535623ddcade568704f7
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-14 09:12:28 +0200

    Update text.d

--- source/uim/bootstrap/bs5/basic/components/forms/text.d
+++ source/uim/bootstrap/bs5/basic/components/forms/text.d
@@ -7,7 +7,7 @@ class DBS5FormText : DBS5Obj {
 	mixin(H5This!("SMALL", ["form-text"]));
 }
 mixin(H5Calls!"BS5FormText");
-///
+
 unittest {
-	assert(BS5FormText, `<small class="form-text"></small>`);
+	assert(BS5FormText == `<small class="form-text"></small>`);
 }
