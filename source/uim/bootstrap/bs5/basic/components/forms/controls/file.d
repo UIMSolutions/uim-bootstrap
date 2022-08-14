5c618a81a4abe2e39ce9273118e8c71530d2c541
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-14 09:48:09 +0200

    Update file.d

--- source/uim/bootstrap/bs5/basic/components/forms/controls/file.d
+++ source/uim/bootstrap/bs5/basic/components/forms/controls/file.d
@@ -5,8 +5,12 @@ import uim.bootstrap;
 
 class DBS5InputFile : DBS5Input {
 	mixin(H5This!(null, ["form-control"], `["type":"file"]`));
+
+  override void initialize () {
+    super.initialize();
+  }
 }
 mixin(H5Calls!("BS5InputFile"));
 unittest {
-	assert(BS5InputFile, `<input class="form-control" type="file">`);
+	assert(BS5InputFile == `<input class="form-control" type="file">`);
 }
