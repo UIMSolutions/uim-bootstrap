f112dc8218a18e745bed0a3df4b76ca72d52a23b
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-14 10:06:04 +0200

    Update range.d

--- source/uim/bootstrap/bs5/basic/components/forms/controls/range.d
+++ source/uim/bootstrap/bs5/basic/components/forms/controls/range.d
@@ -4,9 +4,14 @@
 import uim.bootstrap; 
 
 class DBS5InputRange : DBS5Input {
-	mixin(H5This!(null, null, `["type":"range"]`));
+  mixin(H5This!(null, null, `["type":"range"]`));
+
+  override void initialize () {
+    super.initialize();
+  }
 }
 mixin(H5Calls!("BS5InputRange"));
+
 unittest {
-	assert(BS5InputRange == `<input class="form-control" type="range">`);	
+  assert(BS5InputRange == `<input class="form-control" type="range">`);	
 }
