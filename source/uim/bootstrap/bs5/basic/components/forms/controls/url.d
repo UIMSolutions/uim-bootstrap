4d7c36fe8c8fc155d3cca96f529ea6fca363d4c4
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-14 10:17:26 +0200

    Update url.d

--- source/uim/bootstrap/bs5/basic/components/forms/controls/url.d
+++ source/uim/bootstrap/bs5/basic/components/forms/controls/url.d
@@ -4,10 +4,14 @@
 import uim.bootstrap; 
 
 class DBS5InputUrl : DBS5Input {
-	mixin(H5This!("", null, `["type":"url"]`));
+  mixin(H5This!("", null, `["type":"url"]`));
+
+  override void initialize () {
+    super.initialize();
+  }
 }
 mixin(H5Calls!("BS5InputUrl"));
-///
+ 
 unittest {
-	assert(BS5InputUrl, `<input class="form-control" type="url">`);
+   assert(BS5InputUrl == `<input class="form-control" type="url">`);
 }
