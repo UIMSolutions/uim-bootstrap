dc448f87bf96a12513bf69978813cd29a5e0186b
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-14 09:25:00 +0200

    Update checkbox.d

--- source/uim/bootstrap/bs5/basic/components/forms/controls/checkbox.d
+++ source/uim/bootstrap/bs5/basic/components/forms/controls/checkbox.d
@@ -4,10 +4,10 @@
 import uim.bootstrap; 
 
 class DBS5Checkbox : DBS5Input {
-	mixin(H5This!(null, null, `["type":"checkbox"]`));
+  mixin(H5This!(null, null, `["type":"checkbox"]`));
 }
 mixin(H5Calls!("BS5Checkbox"));
-///
+
 unittest {
-	assert(BS5Checkbox, `<input class="form-control" type="checkbox">`);
+  assert(BS5Checkbox == `<input class="form-control" type="checkbox">`);
 }
