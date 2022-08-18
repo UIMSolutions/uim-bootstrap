26b73d0ba20b31de345cdec9e02a528a15cf723a
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-08-18 09:27:26 +0200

    FEAT: Add color

--- source/uim/bootstrap/bs5/basic/components/forms/controls/input.d
+++ source/uim/bootstrap/bs5/basic/components/forms/controls/input.d
@@ -4,17 +4,27 @@
 import uim.bootstrap; 
 
 class DBS5Input : DBS5Obj {
-	mixin(H5This!("Input", ["form-control"], null, true));	
+	mixin(H5This!("Input", null, null, true));	
 
 	mixin(MyAttribute!"value");
 	mixin(MyAttribute!"placeholder");
 
+	O color(this O)(string newColor) {
+		this.addClasses("bg-"~newColor);
+		return cast(O)this;
+	}
+
   override void initialize () {
     super.initialize();
+
+		this
+			.tag("input")
+			.single(true)
+			.classes("form-control");
   }
 }
 mixin(H5Calls!("BS5Input"));
 
 version(test_uim_bootstrap) { unittest {
-	// TODO assert 
+	assert(BS5Input);
 }}
