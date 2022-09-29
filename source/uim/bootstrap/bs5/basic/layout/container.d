e9f84de66fd3db37be2392f22ccfa311d3e0a79b
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-09-29 11:23:01 +0200

    Fixed

--- source/uim/bootstrap/bs5/basic/layout/container.d
+++ source/uim/bootstrap/bs5/basic/layout/container.d
@@ -16,14 +16,17 @@ class DBS5Container : DBS5Obj {
 	}}
 
 	O fluid(this O)(bool mode = true) { if (mode) _classes = _classes.sub("container").add("container-fluid");  return cast(O)this; }
-	version(test_uim_bootstrap) { unittest {
-		assert(BS5Container.fluid(true) == `<div class="container-fluid"></div>`);
-	}}
+
+	O mode(this O)(string mode) { if (mode.length > 0) _classes = _classes.sub("container").add("container-"~mode);  return cast(O)this; }
 
 	mixin(MyContent!("row", "BS5Row"));
 }
 mixin(H5Calls!("BS5Container"));
 
 version(test_uim_bootstrap) { unittest {
+	assert(BS5Container == `<div class="container"></div>`);
+	assert(BS5Container.fluid(true) == `<div class="container-fluid"></div>`);
+	assert(BS5Container.mode("fluid") == `<div class="container-fluid"></div>`); // Alternative
+	assert(BS5Container.mode("xl") == `<div class="container-xl"></div>`);
 	assert(BS5Container.row == `<div class="container"><div class="row"></div></div>`);
 }}
